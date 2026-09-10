3ce318751d64afb1f0fa5ff07d66a78f9ca460b4
kuddy <31361492+kuddy-on@users.noreply.github.com>
2026-09-10 16:59:21 +0800

    test: exclude protocol table from legacy migration fixtures
    Signed-off-by: kuddy <31361492+kuddy-on@users.noreply.github.com>

--- internal/center/migrations_test.go
+++ internal/center/migrations_test.go
@@ -1380,6 +1380,7 @@ func createLegacyVersion3Database(t *testing.T, directory string) {
 	}
 	defer tx.Rollback()
 	for _, statement := range []string{
+		`DROP TABLE three_x_ui_node_protocols`,
 		`DROP TABLE cloudflare_access_settings`,
 		`DROP TABLE reality_security_checks`,
 		`DROP TABLE node_listener_states`,
